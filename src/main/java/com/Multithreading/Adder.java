c2f5675678f4a89ca7a5e2e999d79d3e3e828155
Emily Bowe <ebowe@tcd.ie>
2020-04-01 21:24:13 +0100

    Single threading

---
+++ src/main/java/com/Multithreading/Adder.java
@@ -0,0 +1,27 @@
+package com.Multithreading;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Adder {
+    private String inFile, outFile;
+
+    public Adder(String inFile, String outFile) {
+
+    }
+    public void doAdd() throws IOException {
+        int total = 0;
+        String line = null;
+
+        try(BufferedReader reader = Files.newBufferedReader(Paths.get(inFile))) {
+            while((line = reader.readLine()) != null)
+                total += Integer.parseInt(line);
+        }
+        try(BufferedWriter writer = Files.newBufferedWriter(Paths.get(outFile))) {
+            writer.write("Total: " + total);
+        }
+    }
+}
--- src/main/java/com/Multithreading/Main.java
+++ src/main/java/com/Multithreading/Main.java
@@ -1,8 +1,22 @@
 package com.Multithreading;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// try with resources
+
+        String[] inFiles = {"./file1.txt", "./file2.txt", "./file3.txt", "./file4.txt", "./file5.txt", "./file6.txt"};
+        String[] outFiles = {"./file1.out.txt", "./file2.out.txt", "./file3.out.txt", "./file4.out.txt", "./file5.out.txt", "./file6.out.txt"};
+
+        try {
+            for(int i = 0; i < inFiles.length; i++) {
+                Adder adder = new Adder(inFiles[i], outFiles[i]);
+                adder.doAdd();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
