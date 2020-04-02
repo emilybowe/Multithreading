cc3e7901661b72a3af457add12a59636d55a2373
Emily Bowe <ebowe@tcd.ie>
2020-04-02 15:48:54 +0100

    If the main thread finishes its work it will kill all the background threads - and they may have uncompleted work.
    1) Instead of local variable, create an array to store references to multiple threads.
    
    2) Then use .join() to block main thread until background threads finish, ensures they can finish their work

--- src/main/java/com/Multithreading/Main.java
+++ src/main/java/com/Multithreading/Main.java
@@ -8,15 +8,21 @@ public class Main {
     //a case for multithreading
     //Threading Foundation: Runnable interface & Thread class
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         String[] inFiles = {"./file1.txt", "./file2.txt", "./file3.txt", "./file4.txt", "./file5.txt", "./file6.txt"};
         String[] outFiles = {"./file1.out.txt", "./file2.out.txt", "./file3.out.txt", "./file4.out.txt", "./file5.out.txt", "./file6.out.txt"};
 
+        Thread[] threads = new Thread[inFiles.length];
+
         for(int i = 0; i < inFiles.length; i++) {
             Adder adder = new Adder(inFiles[i], outFiles[i]);
-            Thread thread = new Thread(adder);
-            thread.start();
+            threads[i] = new Thread(adder);
+            threads[i].start();
+        }
+
+        for(Thread thread: threads) {
+            thread.join();
         }
     }
 }
