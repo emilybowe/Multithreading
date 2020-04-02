63966a003ffb4d62526e998db0a390faf25681aa
Emily Bowe <ebowe@tcd.ie>
2020-04-02 10:33:09 +0100

    Multithreading enabled - using Direct handling

--- src/main/java/com/Multithreading/Main.java
+++ src/main/java/com/Multithreading/Main.java
@@ -3,20 +3,20 @@ package com.Multithreading;
 import java.io.IOException;
 
 public class Main {
+    //opening, reading, writing to files reply on IO, rather than CPU
+    //the thread is therefore not taking advantage of CPU resources
+    //a case for multithreading
+    //Threading Foundation: Runnable interface & Thread class
 
     public static void main(String[] args) {
-	// try with resources
 
         String[] inFiles = {"./file1.txt", "./file2.txt", "./file3.txt", "./file4.txt", "./file5.txt", "./file6.txt"};
         String[] outFiles = {"./file1.out.txt", "./file2.out.txt", "./file3.out.txt", "./file4.out.txt", "./file5.out.txt", "./file6.out.txt"};
 
-        try {
-            for(int i = 0; i < inFiles.length; i++) {
-                Adder adder = new Adder(inFiles[i], outFiles[i]);
-                adder.doAdd();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        for(int i = 0; i < inFiles.length; i++) {
+            Adder adder = new Adder(inFiles[i], outFiles[i]);
+            Thread thread = new Thread(adder);
+            thread.start();
         }
     }
 }
